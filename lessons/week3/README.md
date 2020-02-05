52a3c4f1d6fd17b7040a674c8b0a58b76e3bbc5a
William Adams <Wiladams@users.noreply.github.com>
2020-02-05 06:43:31 -0800

    adding Point2D struct Adding triangle operations

---
+++ lessons/week3/README.md
@@ -0,0 +1,38 @@
+Focus
+=====
+
+Structures II, more focus on data structures as classes.
+
+Keywords
+--------
+
+Object Oriented Programming
+---------------------------
+Encapsulation
+Polymorphism
+Abstraction
+Inheritance
+
+    Class
+    Object
+    Constructor
+    Destructor
+    Inheritance
+
+Extras
+------
+Exploring 2D graphics routines, and rudimentary UI windows.
+
+Resources
+---------
+
+C++ tutorials
+-------------
+https://www.tutorialspoint.com/cplusplus/index.htm
+
+Graphics Specific
+-----------------
+https://www.joshbeam.com/articles/triangle_rasterization/
+https://www.scratchapixel.com/
+
+https://www.scratchapixel.com/lessons/3d-basic-rendering/rasterization-practical-implementation/rasterization-stage
--- lessons/week3/solutions/grtypes.hpp
+++ lessons/week3/solutions/grtypes.hpp
@@ -29,18 +29,91 @@ typedef float coord;
 typedef uint32_t GRSIZE;
 typedef uint16_t GRCOORD;
 
-struct GRTriangle  {
-  GRCOORD x1, y1;
-  GRCOORD x2, y2;
-  GRCOORD x3, y3;
+// Declaration of 2D Point structure
+// This is convenient when representing multiple
+// coordinates in an array in particular
+struct Point2D {
+    GRCOORD x, y;
+    
+    // Convenient constructor, but if you create this constructor,
+    // you MUST then explicitly create a default constructor as well
+    Point2D(GRCOORD ax, GRCOORD ay) : x(ax), y(ay) {}
 
-  GRTriangle(GRCOORD x1, GRCOORD y1,
-    GRCOORD x2, GRCOORD y2,
-    GRCOORD x3, GRCOORD y3)
-    {
-      // sort the coordinates
+    // default constructor
+    Point2D() :x(0), y(0) {}
+
+
+    Point2D operator + (const Point2D &b) {return Point2D{this->x + b.x, this->y + b.y};}
+    Point2D operator - (const Point2D &b) {return Point2D{this->x - b.x, this->y - b.y};}
+    Point2D operator - () {return Point2D(-x, -y);}
+};
+
+
+
+/*
+// Find the vertex which has the smallest 'y' value, which puts it at
+// the 'top' of the screen when the origin is 0,0 topleft.
+inline int FindTopmostPolyVertex(const Point2D *poly, const size_t nelems)
+{
+    int ymin = INT32_MAX;
+    int vmin = 0;
+
+    size_t idx = 0;
+    while (idx < nelems) {
+      if (poly[idx].y < ymin) {
+          ymin = poly[idx].y;
+          vmin = idx;
+      }
+      idx++;
     }
 
+    return vmin;
+}
+*/
+
+
+/*
+  GRTriangle
+
+  A geometric triangle
+*/
+struct GRTriangle  {
+    Point2D verts[3];   // A triangle has 3 vertices
+
+    // Basic constructor take coordinates in any order, sorts them
+    // so that subsequent drawing will be faster.
+    GRTriangle(GRCOORD x1, GRCOORD y1,
+      GRCOORD x2, GRCOORD y2,
+      GRCOORD x3, GRCOORD y3)
+    {
+
+      verts[0] = {x1, y1};
+      verts[1] = {x2, y2};
+      verts[2] = {x3, y3};
+
+      // sort the coordinates from topmost
+      // to bottommost, so drawing is easier
+      // This is essentially a 3 element bubble sort
+      Point2D tmp;
+      if (verts[0].y > verts[1].y) {
+        tmp = verts[0];
+        verts[0] = verts[1];
+        verts[1] = tmp;
+      }
+
+      if (verts[1].y > verts[2].y) {
+        tmp = verts[1];
+        verts[1] = verts[2];
+        verts[2] = tmp;
+      }
+
+      // One more round to ensure the second isn't the smallest
+      if (verts[0].y > verts[1].y) {
+        tmp = verts[0];
+        verts[0] = verts[1];
+        verts[1] = tmp;
+      }
+    }
 } ;
 
 // Some useful routines
@@ -52,3 +125,4 @@ struct GRTriangle  {
 // and 1 represents true
 inline int sgn(REAL val) { return ((0 < val) - (val < 0)); }
 #define swap16(a, b) { int16_t t = a; a = b; b = t; }
+
