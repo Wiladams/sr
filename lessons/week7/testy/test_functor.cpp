dae1614b83df8102f088a3ed785418235028ba52
William Adams <wiladams@microsoft.com>
2020-03-01 07:21:11 -0800

    playing with IEnumerable

--- lessons/week7/testy/test_functor.cpp
+++ lessons/week7/testy/test_functor.cpp
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+template <typename T>
+struct IEnumerator {
+    virtual bool moveNext() = 0;
+    virtual void reset() = 0;
+    T getCurrent()
+    {
+        return T();
+    }
+};
+
+template <typename T>
+struct IIterable {
+    virtual bool isEmpty() const = 0;
+    virtual T& next() = 0;
+    virtual IEnumerator<T> GetEnumerator() = 0;
+
+};
+
 struct AFunction
 {
     void operator()(void)
@@ -7,6 +25,14 @@ struct AFunction
         printf("FUNCTOR\n");
     }
 };
+
+void each(AFunction aFunc, IIterable &aIterator)
+{
+    while (!aIterator.empty())
+        {
+            // perform aFunc on each item from iterator
+        }
+}
 
 void main()
 {
