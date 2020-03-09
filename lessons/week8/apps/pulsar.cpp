77c476c990a92d88911f7f8f5cf20dba3494e13c
William Adams <Wiladams@users.noreply.github.com>
2020-03-08 21:44:10 -0700

    playing around with time based pulsed graphics

---
+++ lessons/week8/apps/pulsar.cpp
@@ -0,0 +1,42 @@
+#include "p5.hpp"
+
+StopWatch sw;
+
+void draw()
+{
+    background(0x7f);
+
+    double scale = 1.0;
+
+    double t = sw.seconds();
+    if (t>0.25) {
+        sw.reset();
+        t = 0.0;
+    }
+
+    double x = MAP(t, 0, 0.25, 0, HALF_PI);
+    double sinx = sin(x);
+    double s = MAP(sinx, 0, 1, 1, 5);
+
+    stroke(0);
+    for (int i = 1;i<=1000;i++)
+    {
+        line(random(width),random(height), random(width),random(height));
+    }
+
+    noStroke();
+    fill(colors.blue);
+    rect(10,10,20,20*s);
+
+    uint8_t a = MAP(s,1,5, 255,50);
+    PixRGBA c = {255,0,0,a};
+    fill(c);
+    rect(mouseX, mouseY, 20*s, 20*s);
+}
+
+void setup()
+{
+    sw.reset();
+    createCanvas(400, 400);
+    frameRate(30);
+}
